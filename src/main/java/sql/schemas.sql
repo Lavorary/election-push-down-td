f664b5e9765008022f2d369e57dbc5bbb97d6608
Lavorary <hei.lavorary@gmail.com>
2026-02-19 11:40:44 +0300

    feat : insert values in tables candidate, voter and vote

--- src/main/java/sql/schemas.sql
+++ src/main/java/sql/schemas.sql
@@ -16,3 +16,23 @@ CREATE TABLE VOTE (
     voter_id INT REFERENCES voter(id),
     vote_type vote_type NOT NULL
 );
+
+--values
+INSERT INTO candidate (name) VALUES
+('Alice'),
+('Bob'),
+('Charlie');
+INSERT INTO voter (name) VALUES
+('Voter1'),
+('Voter2'),
+('Voter3'),
+('Voter4'),
+('Voter5'),
+('Voter6');
+INSERT INTO vote (candidate_id, voter_id, vote_type) VALUES
+(1, 1, 'VALID'),
+(1, 2, 'VALID'),
+(2, 3, 'VALID'),
+(2, 4, 'BLANK'),
+(NULL, 5, 'BLANK'),
+(3, 6, 'NULL');
